73c121351eb130bf87bf4027ffe211fa43233c3e
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-08 18:14:15 +0000

    Switch entrypoint to jupyter lab from jupyter notebook

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -2,4 +2,4 @@
 # entrypoint.sh
 # This script is executed when the Docker container starts.
 
-uv run jupyter notebook --port 8888 --no-browser --ip=0.0.0.0 --notebook-dir app/
+uv run jupyter lab --port "${JUPYTER_PORT:-8888}" --no-browser --ip=0.0.0.0 --notebook-dir app/ --ServerApp.token="${JUPYTER_TOKEN:-}"
