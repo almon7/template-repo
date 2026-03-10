db03827acad028ede8bfd52a58fe8d8c63690084
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-10 08:46:29 +0000

    Remove logging of sample environment variable in main function.
    Leaking env var values in logs is bad practice to bake into a template

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,11 @@
 from loguru import logger
 
 from app.log import configure_logging
-from app.settings import settings
 
 
 def main() -> None:
     configure_logging()
     logger.info("Hello, World!")
-    logger.info(f"Sample Env Var: {settings.sample_env_var}")
 
 
 if __name__ == "__main__":
