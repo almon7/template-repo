e11975f74b64c0639a94fb42c4cb42e1c3319827
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-10 08:24:49 +0000

    Add model_config to Settings to run locally if needed

--- src/app/settings.py
+++ src/app/settings.py
@@ -1,8 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
     sample_env_var: str
+    log_level: str = "INFO"
 
 
 settings = Settings()
